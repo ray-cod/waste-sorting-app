baccbbabf8098e16191c21d0e9e9a03f18dfc043
rlassissi023 <rlassissi023@student.wethinkcode.co.za>
2025-01-18 14:59:19 +0200

    Created WasteCategory model

---
+++ src/main/java/com/enviro/assessment/grad001/raimidikamona/waste_sorting_app/models/RecyclingTip.java
@@ -0,0 +1,4 @@
+package com.enviro.assessment.grad001.raimidikamona.waste_sorting_app.models;
+
+public class RecyclingTip {
+}
---
+++ src/main/java/com/enviro/assessment/grad001/raimidikamona/waste_sorting_app/models/WasteCategory.java
@@ -0,0 +1,84 @@
+package com.enviro.assessment.grad001.raimidikamona.waste_sorting_app.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "waste_category")
+public class WasteCategory {
+
+    // Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 50, message = "Name must not exceed 50 characters")
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Size(max = 255, message = "Description must not exceed 255 characters")
+    @Column(length = 255)
+    private String description;
+
+    @Column(name = "is_recyclable")
+    private Boolean recyclable;
+
+    // Constructor
+    public WasteCategory(Long id, String name, String description, Boolean recyclable) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.recyclable = recyclable;
+    }
+
+    public WasteCategory(String name, String description, Boolean recyclable) {
+        this.name = name;
+        this.description = description;
+        this.recyclable = recyclable;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "Name is required") @Size(max = 50, message = "Name must not exceed 50 characters") String getName() {
+        return name;
+    }
+
+    public void setName(@NotBlank(message = "Name is required") @Size(max = 50, message = "Name must not exceed 50 characters") String name) {
+        this.name = name;
+    }
+
+    public @Size(max = 255, message = "Description must not exceed 255 characters") String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@Size(max = 255, message = "Description must not exceed 255 characters") String description) {
+        this.description = description;
+    }
+
+    public Boolean getRecyclable() {
+        return recyclable;
+    }
+
+    public void setRecyclable(Boolean recyclable) {
+        this.recyclable = recyclable;
+    }
+
+    @Override
+    public String toString() {
+        return "WasteCategory{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", recyclable=" + recyclable +
+                '}';
+    }
+}
