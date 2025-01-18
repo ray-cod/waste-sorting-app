0592db92d45183619eb7c241c5d26987c54029c7
rlassissi023 <rlassissi023@student.wethinkcode.co.za>
2025-01-18 14:21:48 +0200

    Created skeleton

---
+++ src/main/java/com/enviro/assessment/grad001/raimidikamona/waste_sorting_app/WasteSortingAppApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.raimidikamona.waste_sorting_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteSortingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteSortingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/raimidikamona/waste_sorting_app/WasteSortingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.raimidikamona.waste_sorting_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteSortingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
